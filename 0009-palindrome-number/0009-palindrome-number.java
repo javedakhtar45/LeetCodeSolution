61f0a04a70c22f4aeb30b091e726f1247fe7896e
Javed Akhtar <javedakhtar4518@gmail.com>
2026-09-10 18:16:10 +0530

    Time: 5 ms (82.52%), Space: 45.7 MB (86.61%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
+        // if(x<0){
+        //     return false;
+        // }
         int num=x;
         int rev=0;
         while(x>0){
